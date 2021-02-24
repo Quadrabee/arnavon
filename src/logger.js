130dad84ad87ce89beb47920306c7ea1dddc09b7
Louis Lambeau <llambeau@quadrabee.com>
2021-02-24 22:18:34 +0100

    include version number in logs

--- src/logger.js
+++ src/logger.js
@@ -1,8 +1,10 @@
+import { version } from '../package.json';
 import bunyan from 'bunyan';
 
 // Create and use logger in middleware
 const logger = bunyan.createLogger({
   name: 'arnavon',
+  version,
   serializers: bunyan.stdSerializers
 });
 
