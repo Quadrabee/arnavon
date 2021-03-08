5b3c3d970656cd6508d7fc9b73c156f4acb30e4b
Cire DIA <ciremoussadia@gmail.com>
2021-03-08 22:04:07 +0000

    [WIP] moving scripts to makefile

--- bin/deploy_images.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ev
-
-echo "Building image with tag `latest`"
-docker build -t quadrabee/arnavon:latest .  
-
-echo "Pushing image to docker registry"
-docker push quadrabee/arnavon:latest
--- bin/deploy_tagged.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ev
-VERSION=$(shell npm run version --silent)
-
-echo "Building image with tag ${VERSION} "
-docker build --build-arg VERSION=${VERSION} -t quadrabee/arnavon:${VERSION} .
-
-echo "Pushing image to docker registry"
-docker push quadrabee/arnavon:${VERSION}
