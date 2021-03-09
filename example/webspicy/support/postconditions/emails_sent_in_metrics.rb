5f74a8de118eaadb9fce90473b562de83d8374ca
Bernard Lambeau <blambeau@gmail.com>
2021-03-09 21:23:47 +0100

    Enforce coverage of postconditions with webspicy checkboxes.

--- example/webspicy/support/postconditions/emails_sent_in_metrics.rb
+++ example/webspicy/support/postconditions/emails_sent_in_metrics.rb
@@ -3,12 +3,19 @@ class EmailsSentInMetrics
 
   def self.match(service, descr)
     world = service.config.world
+    inc = ->(tc) {
+      if i = tc.metadata[:expected_success_count]
+        i
+      elsif tc.params.is_a?(Array)
+        tc.params.size
+      else
+        1
+      end
+    }
     case descr
-    when /The consumer metrics eventually reflect the mail being sent/
+    when /The consumer metrics eventually reflect the mails? being sent/
       endp = world.devops.workers.endpoint
-      MetricIncremented.new(endp, "runner_successful_jobs", "send-email"){|tc|
-        tc.params.is_a?(Array) ? tc.params.size : 1
-      }
+      MetricIncremented.new(endp, "runner_successful_jobs", "send-email", &inc)
     end
   end
 
